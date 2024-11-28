7904ae73d328e4211bfbf7794773ad29cf35f9a8
SolanaZavr <105708476+Plesh4kov@users.noreply.github.com>
2024-11-28 18:01:07 +0200

    Fix nested repository issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-// pages/_app.tsx
+import { AppProps } from 'next/app'; // Импортируем AppProps для типизации
+
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {  // Типизируем пропсы
   return (
     <div className="min-h-screen bg-gray-100">
       <Component {...pageProps} />
